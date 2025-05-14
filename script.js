9549d3a2435eba0ad5e1f3e9ed0e2cfc53493a06
asst3rix <185900998+asst3rix@users.noreply.github.com>
2025-05-14 11:52:48 +0200

    Mofify createDay(), so it adds a class name for the previous, current and next month days.

--- script.js
+++ script.js
@@ -46,9 +46,10 @@ function getMonthData(year, month) {
 }
 
 // Create a day with his number associated.
-function createDay(numberAssociated) {
+function createDay(numberAssociated, cssClass) {
     const day = document.createElement("div");
     day.textContent = numberAssociated.toString();
+    day.classList.add(cssClass);
     days.appendChild(day);
 }
 
@@ -58,31 +59,33 @@ function createMonth(month) {
         days.firstChild.remove()
     }
 
-    // For the previous month:
+    /**** PREVIOUS MONTH CREATION ****/
     // If month.firstDay === Sunday, we need to show 6 days.
     // Else we need to show (month.firstDay - 1) days.
-    let daysToShow = month.firstDay === 0 ? 6 : month.firstDay - 1;
+    const daysToShow = month.firstDay === 0 ? 6 : month.firstDay - 1;
     // Example here with August 2025:
     // month.totalDaysPreviousMonth = 31
     // daysToShow = 4 (5 (Friday) - 1)
     // startDay = 28.
-    let startDay = month.totalDaysPreviousMonth - daysToShow + 1;
-    for (let i = startDay; i <= month.totalDaysPreviousMonth; i++) {
-        createDay(i);
+    const startDay = month.totalDaysPreviousMonth - daysToShow + 1;
+    for (let dayNumber = startDay; dayNumber <= month.totalDaysPreviousMonth; dayNumber++) {
+        createDay(dayNumber, "previous-month");
     }
 
+    /**** CURRENT MONTH CREATION ****/
     // We create the current month from 1 to month.totalDays.
-    for (let i = 1; i <= month.totalDays; i++) {
-        createDay(i);
+    for (let dayNumber = 1; dayNumber <= month.totalDays; dayNumber++) {
+        createDay(dayNumber, "current-month");
     }
 
+    /**** NEXT MONTH CREATION ****/
     if (month.lastDay !== 0) {
         // We create the next month from 1 to how many days we can fit in the remaining week.
         // We use month.lastDay to calculate how many days we need to create.
         // Example on June 2025:
         // month.lastDay = 1 (Monday), so we need to create 6 days.
-        for (let i = 1; i <= 7 - month.lastDay; i++) {
-            createDay(i);
+        for (let dayNumber = 1; dayNumber <= 7 - month.lastDay; dayNumber++) {
+            createDay(dayNumber, "next-month");
         }
     }
 }
